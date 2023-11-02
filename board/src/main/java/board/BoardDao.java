fec116e3488f945a4229d0e5fd0700668d07d972
admin <admin@admin.com>
2023-11-02 21:03:28 +0900

    first commit

---
+++ board/src/main/java/board/BoardDao.java
@@ -0,0 +1,247 @@
+package board;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import common.JdbcUtil;
+
+public class BoardDao {
+	private JdbcUtil ju;
+	
+	public BoardDao() {
+		ju = JdbcUtil.getInstance();
+	}
+	
+	//삽입(C)
+	public int insert(BoardVO vo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "insert into \"BOARD\"(\"NUM\", \"TITLE\", \"WRITER\", \"CONTENT\", \"REGDATE\", \"CNT\") values (\"BOARD_SEQ\".nextval, ?, ?, ?, sysdate, 0)";
+		int ret = -1;
+		try {
+			con = ju.getConnection();
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getWriter());
+			pstmt.setString(3, vo.getContent());
+			ret = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close(); 
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close(); 
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ret;
+	}
+	//조회(R)
+	public List<BoardVO> selectAll(){
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String query = "select \"NUM\", \"TITLE\", \"WRITER\", \"CONTENT\", \"REGDATE\", \"CNT\" from \"BOARD\" order by \"NUM\" desc";
+		ArrayList<BoardVO> ls = new ArrayList<BoardVO>();
+		try {
+			con = ju.getConnection();
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				BoardVO vo = new BoardVO(
+						rs.getInt(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getString(4),
+						new Date(rs.getDate(5).getTime()),
+						rs.getInt(6));
+				ls.add(vo);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(rs != null) {
+				try {
+					rs.close(); 
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(stmt != null) {
+				try {
+					stmt.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close(); 
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ls;
+	}
+	
+	//조회(R)
+		public BoardVO selectOne(int num){
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String query = "select \"NUM\", \"TITLE\", \"WRITER\", \"CONTENT\", \"REGDATE\", \"CNT\" from \"BOARD\" where \"NUM\"=?";
+			BoardVO vo = null;
+			try {
+				con = ju.getConnection();
+				pstmt = con.prepareStatement(query);
+				pstmt.setInt(1,num);
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					updateCnt(num);//조회수 증가
+					vo = new BoardVO(
+							rs.getInt(1),
+							rs.getString(2),
+							rs.getString(3),
+							rs.getString(4),
+							new Date(rs.getDate(5).getTime()),
+							rs.getInt(6)+1);
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}finally {
+				if(rs != null) {
+					try {
+						rs.close(); 
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(pstmt != null) {
+					try {
+						pstmt.close();
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(con != null) {
+					try {
+						con.close(); 
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			return vo;
+		}
+	//수정(U)
+	public int update(BoardVO vo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "update \"BOARD\" set \"TITLE\"=?, \"CONTENT\"=? where \"NUM\"=?";
+		int ret = -1;
+		try {
+			con = ju.getConnection();
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getContent());
+			pstmt.setInt(3, vo.getNum());
+			ret = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close(); 
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ret;
+	}
+	
+	public int updateCnt(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "update \"BOARD\" set \"CNT\"=\"CNT\"+1 where \"NUM\"=?";
+		int ret = -1;
+		try {
+			con = ju.getConnection();
+			pstmt = con.prepareStatement(query);
+			pstmt.setInt(1, num);
+			ret = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close(); 
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ret;
+	}
+	//삭제(D)
+	public int delete(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "delete from \"BOARD\" where \"NUM\"=?";
+		int ret = -1;
+		try {
+			con = ju.getConnection();
+			pstmt = con.prepareStatement(query);
+			pstmt.setInt(1, num);
+			ret = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close(); 
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ret;
+	}
+}
